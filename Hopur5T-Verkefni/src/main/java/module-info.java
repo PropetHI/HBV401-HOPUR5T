5838ff2dac480e302d334011fbda2420106d4598
PropetHI <124005666+PropetHI@users.noreply.github.com>
2024-03-18 20:29:12 +0000

    Delete Hopur5T-Verkefni directory

--- Hopur5T-Verkefni/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module adrian.roszkowski.hopur5tverkefni {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens adrian.roszkowski.hopur5tverkefni to javafx.fxml;
-    exports adrian.roszkowski.hopur5tverkefni;
-}
